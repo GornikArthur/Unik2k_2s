9e1fa2683172d57f5db26222d194f4f3ec2ed5e5
Gornik Arthur <59534987+GornikArthur@users.noreply.github.com>
2025-02-20 21:02:57 +0300

    20.02

---
+++ МИАПР/Лабораторные работы/MiAPR_1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MiAPR_1_VS
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.button3 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Sitka Text Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(463, 603);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(175, 30);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Число образов:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(467, 632);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(171, 29);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(467, 704);
+			this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(170, 29);
+			this.textBox2.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Sitka Text Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(465, 675);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(172, 30);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Число классов:";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Sitka Text Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(657, 644);
+			this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(240, 61);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Классифицировать";
+			this.button1.UseMnemonic = false;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Sitka Text Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(912, 539);
+			this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(240, 61);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Очистить";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.WindowText;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(897, 600);
+			this.pictureBox1.TabIndex = 8;
+			this.pictureBox1.TabStop = false;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+			this.dataGridView1.Location = new System.Drawing.Point(912, 0);
+			this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowHeadersWidth = 4;
+			this.dataGridView1.Size = new System.Drawing.Size(240, 533);
+			this.dataGridView1.TabIndex = 9;
+			// 
+			// Column1
+			// 
+			this.Column1.HeaderText = "";
+			this.Column1.Name = "Column1";
+			this.Column1.ReadOnly = true;
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Sitka Text Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button3.Location = new System.Drawing.Point(929, 635);
+			this.button3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(189, 94);
+			this.button3.TabIndex = 10;
+			this.button3.Text = "Метод К-средних";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(1163, 741);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Sitka Text Semibold", 12.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "Lab1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ МИАПР/Лабораторные работы/MiAPR_7/MIAPR7_new/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace MiAPR_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_Primitives = new System.Windows.Forms.ComboBox();
+            this.pictureBox_Man = new System.Windows.Forms.PictureBox();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.button_Analysis = new System.Windows.Forms.Button();
+            this.label_Primitives = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Man)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox_Primitives
+            // 
+            this.comboBox_Primitives.FormattingEnabled = true;
+            this.comboBox_Primitives.Location = new System.Drawing.Point(294, 7);
+            this.comboBox_Primitives.Name = "comboBox_Primitives";
+            this.comboBox_Primitives.Size = new System.Drawing.Size(252, 30);
+            this.comboBox_Primitives.TabIndex = 0;
+            // 
+            // pictureBox_Man
+            // 
+            this.pictureBox_Man.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox_Man.Location = new System.Drawing.Point(12, 43);
+            this.pictureBox_Man.Name = "pictureBox_Man";
+            this.pictureBox_Man.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox_Man.TabIndex = 1;
+            this.pictureBox_Man.TabStop = false;
+            this.pictureBox_Man.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_Man_MouseDown);
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Clear.Location = new System.Drawing.Point(490, 649);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(207, 32);
+            this.button_Clear.TabIndex = 2;
+            this.button_Clear.Text = "Очистить поле";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // button_Analysis
+            // 
+            this.button_Analysis.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Analysis.Location = new System.Drawing.Point(116, 649);
+            this.button_Analysis.Name = "button_Analysis";
+            this.button_Analysis.Size = new System.Drawing.Size(207, 32);
+            this.button_Analysis.TabIndex = 3;
+            this.button_Analysis.Text = "Анализ рисунка";
+            this.button_Analysis.UseVisualStyleBackColor = true;
+            this.button_Analysis.Click += new System.EventHandler(this.button_Analysis_Click);
+            // 
+            // label_Primitives
+            // 
+            this.label_Primitives.AutoSize = true;
+            this.label_Primitives.Location = new System.Drawing.Point(29, 10);
+            this.label_Primitives.Name = "label_Primitives";
+            this.label_Primitives.Size = new System.Drawing.Size(182, 22);
+            this.label_Primitives.TabIndex = 4;
+            this.label_Primitives.Text = "Набор примитивов:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(587, -3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(198, 23);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Мороженое в левой руке";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(587, 18);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(208, 23);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "Мороженое в правой руке";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(834, 685);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label_Primitives);
+            this.Controls.Add(this.button_Analysis);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.pictureBox_Man);
+            this.Controls.Add(this.comboBox_Primitives);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "СИНТЕЗ И РАСПОЗНАВАНИЕ ОБЪЕКТОВ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Man)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_Primitives;
+        private System.Windows.Forms.PictureBox pictureBox_Man;
+        private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.Button button_Analysis;
+        private System.Windows.Forms.Label label_Primitives;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+

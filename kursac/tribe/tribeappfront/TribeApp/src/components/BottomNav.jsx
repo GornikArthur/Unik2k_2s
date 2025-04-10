31c6634a85f79838b4407210a3b1fec14ecabe0c
Gornik Arthur <59534987+GornikArthur@users.noreply.github.com>
2025-04-10 12:35:24 +0300

    10.04

--- kursac/tribe/tribeappfront/TribeApp/src/components/BottomNav.jsx
+++ kursac/tribe/tribeappfront/TribeApp/src/components/BottomNav.jsx
@@ -1,10 +1,11 @@
 
+import { Link } from 'react-router-dom';
 function BottomNav() {
     return (
         <nav className="bottom-nav">
-            <a href="#" className="nav-item"><img src="../img/heart.png" alt="Heart Picture"/></a>
-            <a href="#" className="nav-item"><img src="../img/search.png" alt="Heart Picture"/></a>
-            <a href="#" className="nav-item"><img src="../img/Profile.png" alt="Heart Picture"/></a>
+            <Link to="/likes"><a href="#" className="nav-item"><img src="img/heart.png" alt="Heart Picture"/></a></Link>
+            <Link to="/"><a href="#" className="nav-item"><img src="img/search.png" alt="Heart Picture"/></a></Link>
+            <Link to="/edit"><a href="#" className="nav-item"><img src="img/Profile.png" alt="Heart Picture"/></a></Link>
         </nav>
     );
 }
--- kursac/tribe/tribeappfront/TribeApp/src/components/LikeDislike.jsx
+++ kursac/tribe/tribeappfront/TribeApp/src/components/LikeDislike.jsx
@@ -1,8 +1,8 @@
 function LikeDislike() {
     return (
         <div className="like-dislike">
-            <button className="like-dislike-buttons" id="like-btn"><img src="../img/like.png" alt="Like Picture"/></button>
-            <button className="like-dislike-buttons" id="dislike-btn"><img src="../img/dislike.png" alt="Disike Picture"/></button>
+            <button className="like-dislike-buttons" id="like-btn"><img src="img/like.png" alt="Like Picture"/></button>
+            <button className="like-dislike-buttons" id="dislike-btn"><img src="img/dislike.png" alt="Disike Picture"/></button>
         </div>
     );
 }
